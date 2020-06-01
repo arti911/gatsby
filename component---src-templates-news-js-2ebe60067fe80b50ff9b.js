3d47604b4236c43fbaebf2378c049da9857af3dd
Artur <artur.jog@yandex.ru>
2020-06-01 18:46:39 +0400

    Updates

---
+++ component---src-templates-news-js-2ebe60067fe80b50ff9b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{YWiO:function(e,n,t){"use strict";t.r(n),t.d(n,"query",(function(){return i}));var a=t("q1tI"),r=t.n(a),l=t("Wbzz"),u=t("Bl7J"),i="23437317";n.default=function(e){var n=e.data.news.articles;return r.a.createElement(u.a,null,r.a.createElement(l.Link,{to:"/"},"← Назад"),r.a.createElement("pre",null,JSON.stringify(n,null,1)))}}}]);
+//# sourceMappingURL=component---src-templates-news-js-2ebe60067fe80b50ff9b.js.map
